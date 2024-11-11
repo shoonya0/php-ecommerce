58ee03cc291a8ba9d5831fa7202f602b314db173
Your Name <your_email@example.com>
2024-11-11 15:47:10 +0530

    Enhance Application Structure and User Experience
    - New Features:
      - Introduced multiple Livewire components for improved interactivity, including LoginForm, ProductListing, and ProductSection.
      - Added new layouts for both authenticated and guest users to streamline navigation and enhance UI.
    
    - Image Management:
      - Renamed and organized image assets for better accessibility and consistency.
    
    - Component Development:
      - Created reusable Blade components for buttons, input fields, and navigation links to promote code reusability and maintainability.
    
    - Routing Improvements:
      - Updated routing files to accommodate new authentication and product-related features.
    
    - Performance Boost:
      - Integrated Laravel Breeze for authentication, significantly enhancing application performance and user experience.

---
+++ resources/views/livewire/product-listing.blade.php
@@ -0,0 +1,6 @@
+<div class="grid grid-cols-2 lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-3">
+    <livewire:item-card />
+    <livewire:item-card />
+    <livewire:item-card />
+    <livewire:item-card />
+</div>
---
+++ resources/views/livewire/product-section.blade.php
@@ -0,0 +1,17 @@
+<div class="px-10 md:px-20 sm:px-30 py-3">
+    <!-- Brand New -->
+    <h2 class="font-bold text-[20px] my-3">Brand New</h2>
+    <livewire:product-listing />
+
+    <!-- Smartphones & Laptops -->
+    <h2 class="font-bold text-[20px] my-3">Smartphones & Laptops</h2>
+    <livewire:product-listing />
+
+    <!-- Outfits -->
+    <h2 class="font-bold text-[20px] my-3">Outfits</h2>
+    <livewire:product-listing />
+
+    <!-- Digital Products -->
+    <h2 class="font-bold text-[20px] my-3">Digital Products</h2>
+    <livewire:product-listing />
+</div>
